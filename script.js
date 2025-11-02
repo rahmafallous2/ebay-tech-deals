1cbe3faebdfe6faac6a8eaa79001edec1d26beba
rahmafallous2 <rahmifallous@gmail.com>
2025-11-02 20:29:07 +0200

    fix issue

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace <USERNAME> and <REPO> with your actual GitHub username and repository name
-const csvUrl = "https://raw.githubusercontent.com/<USERNAME>/<REPO>/main/bitcoin_hourly_data.csv";
+const csvUrl = "https://raw.githubusercontent.com/rahmafallous2/ebay-tech-deals/main/ebay_tech_deals.csv";
 
 async function fetchCsvData() {
   try {
